8e447f3e185fa06f4b9410584c8bacf57b1e437f
nuuuwan <nuuuwan@gmail.com>
2024-05-23 12:18:24 +0530

    various

--- src/dmc/reports/RiverWaterLevelMap.py
+++ src/dmc/reports/RiverWaterLevelMap.py
@@ -25,9 +25,12 @@ class RiverWaterLevelMap:
         ents = Ent.list_from_type(self.ENT_TYPE)
         rwl_list = RiverWaterLevel.list_from_latest()
 
+        
         fig, ax = plt.subplots()
         fig.set_size_inches(4.5, 8)
-
+        plt.tight_layout()
+        
+        
         for ent in ents:
             max_level = 0
             no_stations = True
@@ -41,30 +44,50 @@ class RiverWaterLevelMap:
                 max_level = max(max_level, rwl.level)
 
             if no_stations:
-                color = '#8884'
+                color = '#8888'
             else:
                 color = ['#080', '#ff0', '#f80', '#f00'][max_level]
-                color = color + '4'
+                color = color + '8'
 
             geo = ent.geo()
-            geo.plot(ax=ax, color=color, edgecolor='white')
+            geo.plot(ax=ax, color=color, edgecolor='#fff')
             ax.grid(False)
 
-        for rwl in rwl_list:
-            station = Station.from_name(rwl.station)
-            if not station:
-                continue
-            color = ['#080', '#ff0', '#f80', '#f00'][rwl.level]
+        for level in range(0, 4):
+            for rwl in rwl_list:
+                station = Station.from_name(rwl.station)
+                if not station:
+                    continue
+                if rwl.level != level:
+                    continue
+                color = ['#080', '#ff0', '#f80', '#f00'][rwl.level]
+                circle = plt.Circle(
+                    (station.latLng.lng, station.latLng.lat), 0.03, facecolor=color,edgecolor="black"
+                )
+                ax.add_patch(circle)
+                plt.text(
+                    station.latLng.lng+0.05,
+                    station.latLng.lat-0.015,
+                    station.name,
+                    fontsize=5,
+                )
+
+        for level in range(0, 4):
+            lng = 81
+            lat = 9.5 - 0.1 * level
+            color = ['#080', '#ff0', '#f80', '#f00'][level]
             circle = plt.Circle(
-                (station.latLng.lng, station.latLng.lat), 0.03, color=color
+                (lng, lat), 0.03, facecolor=color,edgecolor="black"
             )
             ax.add_patch(circle)
+            label = ['Normal', 'Alert', 'Minor Flood', "Major Flood"][level]
             plt.text(
-                station.latLng.lng,
-                station.latLng.lat,
-                station.name,
+                lng+0.05, lat-0.015,
+                label,
                 fontsize=5,
-            )
+            )    
+        
+
         ax.set_xticks([])
         ax.set_yticks([])
 
