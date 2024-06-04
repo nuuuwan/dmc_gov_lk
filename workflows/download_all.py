cb73fb478175bb34e3226cc40df3703abc444a2e
Nuwan I. Senaratna <nuuuwan@gmail.com>
2024-06-04 14:36:56 +0300

    Update download_all.py

--- workflows/download_all.py
+++ workflows/download_all.py
@@ -3,12 +3,15 @@ from dmc import GenericDownloader, RiverWaterLevel
 
 def main():
     for doc_type, report_type_id in [
-        ['situation-reports', 1],
+        # common
+        [RiverWaterLevel.DOC_TYPE, 6],
+        ['landslide-warnings', 5],
         ['weather-reports', 2],
+        ['situation-reports', 1],
+        
+        # less common
         ['flood-inundation-maps', 3],
         ['emergency-response-data-viewer', 4],
-        ['landslide-warnings', 5],
-        [RiverWaterLevel.DOC_TYPE, 6],
         ['earthquakes', 7],
     ]:
         downloader = GenericDownloader(doc_type, report_type_id)
