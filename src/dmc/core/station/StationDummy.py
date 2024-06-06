d24dfe152525f258757d3fc28e92a598d67671b9
nuuuwan <nuuuwan@gmail.com>
2024-06-06 17:12:48 +0530

    Added name-to-district-id.json

--- src/dmc/core/station/StationDummy.py
+++ src/dmc/core/station/StationDummy.py
@@ -1,9 +1,14 @@
-from utils_future import LatLng
-from utils import JSONFile
 import os
+
+from utils import JSONFile
+
+from utils_future import LatLng
+
 
 class StationDummy:
     @classmethod
     def list_all_dummy(cls):
-        idx = JSONFile(os.path.join('data-static', 'station-dummy.json')).read()
+        idx = JSONFile(
+            os.path.join('data-static', 'station-dummy.json')
+        ).read()
         return [cls.dummy(k, LatLng(v[0], v[1])) for k, v in idx.items()]
